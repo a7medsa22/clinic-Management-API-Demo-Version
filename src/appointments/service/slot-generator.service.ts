a98bd877f612198ca0f118b4d37ed12c099f0011
Ahmed Salah Fathy Sotohy <laslsalsa123@gmail.com>
2026-05-04 23:31:21 +0300

    feat: Enhance SlotGeneratorService to generate time slots based on doctor's availability and day of the week

--- src/appointments/service/slot-generator.service.ts
+++ src/appointments/service/slot-generator.service.ts
@@ -1,4 +1,5 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
+import { DayOfWeek } from "@prisma/client";
 import { min } from "class-validator";
 import { PrismaService } from "src/prisma/prisma.service";
 interface TimeSlot {
@@ -44,7 +45,7 @@ export class SlotGeneratorService {
             }
         });
         const doctorDayDates = new Set(
-            dayOff.map(d=>d.date.toISOString().split('T')[0])
+            dayOff.map(d => d.date.toISOString().split('T')[0])
         );
         // 4. Get already booked appointments
         const appointments = await this.prisma.appointment.findMany({
@@ -57,10 +58,75 @@ export class SlotGeneratorService {
             select: { startTime: true, endTime: true }
         });
         // 5. Generate slots for each day in range
+        const currentDate = new Date(startDate);
+        currentDate.setHours(0, 0, 0, 0);
+
+        while (currentDate <= endDate) {
+            const dayOfWeek = this.getDayOfWeek(currentDate)
+
+            const dayAvailabilities = availabilities.filter(
+                d => d.dayOfWeek == dayOfWeek
+            );
+
+            for (const availability of dayAvailabilities) {
+                const daySlots = this.generateDaySlots(
+                    currentDate,
+                    availability.startTime,
+                    availability.endTime,
+                    availability.slotDuration
+                )
+                slots.push(...daySlots);
+            }
+            currentDate.setDate(currentDate.getDay() + 1);
+        }
+        return slots;
+    }
+    generateDaySlots(
+        date: Date,
+        startMin: number,
+        endMin: number,
+        slotDuration: number,
+
+    ): TimeSlot[] {
+        const slots: TimeSlot[] = [];
+        let current = startMin;
+
+        while (current + slotDuration <= endMin) {
+            let startSlots = this.minutesToDate(date, startMin);
+            let endSlots = this.minutesToDate(date, startMin + slotDuration);
+            slots.push({
+                start: startSlots,
+                end: endSlots
+            });
+            current += slotDuration;
+        }
+        return slots
+    }
 
 
 
-
+    private getDayOfWeek(date: Date) {
+        const dayNumber = date.getDate()
+        const dayMap: { [key: number]: DayOfWeek } = {
+            0: DayOfWeek.SUNDAY,
+            1: DayOfWeek.MONDAY,
+            2: DayOfWeek.TUESDAY,
+            3: DayOfWeek.WEDNESDAY,
+            4: DayOfWeek.THURSDAY,
+            5: DayOfWeek.FRIDAY,
+            6: DayOfWeek.SATURDAY,
+        }
+        return dayMap[dayNumber]
+    }
+    private timeToMinutes(time: string) {
+        const [hours, minute] = time.split(':').map(Number);
+        return hours * 60 + minute;
+    }
+    private minutesToDate(date: Date, minutes: number) {
+        const result = new Date(date);
+        result.setHours(0, 0, 0, 0);
+        result.setMinutes(minutes)
+        return result;
     }
     isValidTimeFormat(timeStr: string): boolean {
         const regex = /^([01][0-9]|2[0-3]):([0-5][0-9])$/;
